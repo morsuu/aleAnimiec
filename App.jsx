c3c8adcfc1713f3a8d4786e5e1547c44daea61dc
morsu <morsu006@gmail.com>
2026-01-24 18:48:19 +0100

    Aktualizacja adresu backendu

--- App.jsx
+++ App.jsx
@@ -3,7 +3,7 @@ import ReactPlayer from 'react-player';
 import io from 'socket.io-client';
 
 // Pamiętaj o zmianie na adres produkcyjny (Render) po wdrożeniu
-const SOCKET_URL = 'http://localhost:3002';
+const SOCKET_URL = 'https://aleanimiec-backend.onrender.com/';
 const socket = io(SOCKET_URL);
 
 function App() {
